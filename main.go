01a1a215bf26f39fd269f349f8c211b19e7e2c96
WebDev TVlean <tvleanjohnny@gmail.com>
2023-09-12 17:56:41 +0300

    new files

--- main.go
+++ main.go
@@ -1,29 +1,33 @@
 package main
 
 import (
-	"fmt"      /*Пакет вывода на экран*/
-	"net/http" /*Пакет http*/
+	"fmt"           /*Пакет вывода на экран*/
+	"html/template" /* Пакет вывода html страниц */
+	"net/http"      /*Пакет http*/
 )
 
 type User struct {
-	name                  string
-	age                   uint16 /* Целове число не может быть отрицательным */
-	money                 int16
-	avg_grades, happiness float64 /* Число с плавающей точкой */
+	Name                  string
+	Age                   uint16 /* Целове число не может быть отрицательным */
+	Money                 int16
+	Avg_grades, Happiness float64 /* Число с плавающей точкой */
+	Hobbies               []string
 }
 
 func (u User) getAllInfo() string {
 	return fmt.Sprintf("Ползователь %s имеет возраст %d и зарпралту в"+
-		"размере %d.", u.name, u.age, u.money)
+		"размере %d.", u.Name, u.Age, u.Money)
 }
 
 func (u *User) setNewName(newName string) {
-	u.name = newName
+	u.Name = newName
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-	// bob := User{"Bob", 25, -50, 4.4, 0.8}
-	fmt.Fprintf(w, "<h1>Title</h1>")
+	bob := User{"Bob", 25, -50, 4.4, 0.8, []string{"Footbal", "Basketball", "Skate", "Dance"}}
+	//fmt.Fprintf(w, "<h1>Title</h1>")
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, bob)
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
@@ -33,6 +37,8 @@ func contacts_page(w http.ResponseWriter, r *http.Request) {
 func handleRequest() {
 	http.HandleFunc("/", home_page)
 	http.HandleFunc("/contacts/", contacts_page)
+	// Подключение папки assets
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 	http.ListenAndServe(":8080", nil)
 }
 
