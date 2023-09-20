0a144fad875758ab2447753f886ada1c71476150
WebDev TVlean <tvleanjohnny@gmail.com>
2023-09-20 17:17:12 +0300

    add new page and connect to db

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"           /*Пакет вывода на экран*/
 	"html/template" /* Пакет вывода html страниц */
 	"net/http"      /*Пакет http*/
+
+	/* Пакет работы с SQL */
+
+	_ "github.com/go-sql-driver/mysql" /* Пакет работы с SQL */
 )
 
 type User struct {
@@ -23,6 +28,11 @@ func (u *User) setNewName(newName string) {
 	u.Name = newName
 }
 
+func index(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("templates/index.html")
+	tmpl.Execute(w, r)
+}
+
 func home_page(w http.ResponseWriter, r *http.Request) {
 	bob := User{"Bob", 25, -50, 4.4, 0.8, []string{"Footbal", "Basketball", "Skate", "Dance"}}
 	//fmt.Fprintf(w, "<h1>Title</h1>")
@@ -34,9 +44,26 @@ func contacts_page(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Contacts easy!!")
 }
 
+func pages_sign_up(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("templates/pages-sign-up.html")
+	tmpl.Execute(w, r)
+}
+func pages_sign_in(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("templates/pages-sign-in.html")
+	tmpl.Execute(w, r)
+}
+func pages_profile(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("templates/pages-profile.html")
+	tmpl.Execute(w, r)
+}
+
 func handleRequest() {
-	http.HandleFunc("/", home_page)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/home_page/", home_page)
 	http.HandleFunc("/contacts/", contacts_page)
+	http.HandleFunc("/pages-sign-up/", pages_sign_up)
+	http.HandleFunc("/pages-sign-in/", pages_sign_in)
+	http.HandleFunc("/pages-profile/", pages_profile)
 	// Подключение папки assets
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 	http.ListenAndServe(":8080", nil)
@@ -46,5 +73,23 @@ func main() {
 	//var bob User = ....
 	//bob := User{ name : "Bob", age: 25, money: -50, avg_grades: 4.4, happiness: 0.8 }
 	//bob := User{ "Bob", 25, -50, 4.4, 0.8 }
+
+	db, err := sql.Open("mysql", "cowboy_kpda:22031986Te!@tcp(cowboy.beget.tech)/cowboy_kpda")
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Установка данных
+
+	insert, err := db.Query("INSERT INTO `users` (`name`,`age`) VALUES ('Bob', 35)")
+	if err != nil {
+		panic(err)
+	}
+	defer insert.Close()
+
+	fmt.Println("Подключено в БД")
+
 	handleRequest()
 }
